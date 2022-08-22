92d8159f58bbc1b61de7f51e6fe8e63ce9bf2eaa
aiman <aimannigar.13@gmail.com>
2022-08-22 10:12:03 -0700

    tailwind file

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["*"],
+    theme: {
+        extend: {
+            grey: "#EDEEF1",
+            blue: "#00BAF2",
+        },
+    },
+    plugins: [],
+}
